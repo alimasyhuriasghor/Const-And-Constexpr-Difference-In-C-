b05443b5b8b51c0e973809b061843865a4c7e734
alimasyhuriasghor <alimasyhuriasghor6@gmail.com>
2023-09-03 03:18:51 +0700

    Fix Some Line Of Comments And Code

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ constexpr int sum(const int x, const int y) {
 
 int main(int argc, char const *argv[])
 {
-    // Constant expression is an expression that can be evaluated during compilation time.to be a constant expression all the values must be known by the compiler at compilation time
+    // Constant expression is an expression that can be evaluated during compilation time.to be a constant expression all the values must be constant and known by the compiler at compilation time
 
     // Runtime constants are whose initialization values are not known during compilation time and Runtime constant values didn't evaluated at compilation time and aren't know at compilation time
 
@@ -29,6 +29,12 @@ int main(int argc, char const *argv[])
 
     // Note that not all constant value will be known during compilation time constant value could be either a Compile Time Constant or Run Time Constant..evaluating constant expression during compilation time will make our compilation time takes long because our compiler has to do a lot of works like calculating a result but such expressions only needed to evaluated or calculated once rather than everytime we run our programs.the resulting executables are faster and use less memory
 
+    /*
+    Note That Any variable that should not be modifiable after initialization and whose initializer is known at compile-time should be declared as constexpr.
+
+    Note That Any variable that should not be modifiable after initialization and whose initializer is not known at compile-time should be declared as const.
+    */
+
     // for instance
     // let's say that i have a simple variable that multiplies both value
     int input_user;
@@ -37,16 +43,16 @@ int main(int argc, char const *argv[])
     std::cout << "Insert any numbers that is not negative number: ";
     std::cin >> input_user; // this is also a Runtime constant and the value cannot be known during compilation time because the input user is not constant expression
 
-    std::string result1 = (input_user <= 0) ? "False input program must be positive number!!" : "Correct you're inserting a positive number"; // this is also Runtime constant and the value cannot be known during compilation time because the result1 is not constant
+    const std::string result1 = (input_user >= 0) ? "Correct you're inserting a positive number" : "Incorrect you're inserting a negative number!!"; // <- this is also a Runtime constant whose values aren't known by the compiler
  
     PRINT(result1); // <- note that after i am printing the value to the console it will still a constant run time not compile time constant the reason why constexpr or const useful are useful is to know the value and evaluated the value during Compile Time not during Runtime
 
 
-    const std::string name{"Gerry"}; // <- the value of name "Gerry" will be known at compilation time
-    const int sum2{5 + 5}; // <- the sum of 5 + 5 will be evaluated or calculated during Compilation Time not Runtime Constant
+    const std::string name{"Gerry"}; // <- this is a Compile Time Constant.the value of name "Gerry" will be known at compilation time
+    const int sum2{5 + 5}; // <- this is a Compile Time Constant.the sum of 5 + 5 will be evaluated or calculated during Compilation Time not Runtime Constant
 
-    constexpr int num1 = 5; // <- all variables that is declared with constexpr will be known by the compiler
-    constexpr int num2 = 6; // <- all variables that is declared with constexpr will be known by the compiler
+    constexpr int num1 = 5; // <- all variables that is declared with constexpr will automaticlly known by the compiler
+    constexpr int num2 = 6; // <- all variables that is declared with constexpr will automaticlly known by the compiler
     constexpr int result2 = (num1 > num2) ? num1 : num2; // <- all variables that is declared with constexpr will be known by the compiler
 
     // Function Constant Expression Result
